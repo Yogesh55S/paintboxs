3f11e7749f4b666f539d57587e55b42ae44efc35
Yogesh55S <Yogesharma914@gmail.com>
2025-11-27 13:55:31 +0530

    fnext

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import RecentWork from "./recent-work/page";
-const desktopImages = [
-  "/images/im.jpg",
-  "/images/im2.jpg",
-  "/images/im3.jpg",
-];
-
-const mobileImages = [
-  "/images/im.jpg",
-  "/images/im2.jpg",
-  "/images/im3.jpg",
-];
-
 const desktopBg = "/images/img.jpg";
 const mobileBg = "/images/download.jpg";
 
@@ -21,14 +9,8 @@ export default function Home() {
     <>
       <Navbar />
       <main>
-        <Hero
-          desktopImages={desktopImages}
-          mobileImages={mobileImages}
-          desktopBg={desktopBg}
-          mobileBg={mobileBg}
-        />
-       
-      </main> 
+        <Hero desktopBg={desktopBg} mobileBg={mobileBg} />
+      </main>
       <RecentWork/>
     </>
   );
